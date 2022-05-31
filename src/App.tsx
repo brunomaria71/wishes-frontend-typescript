d9352cf78ca6df1ba3aa250cf79cdf99ba109725
brunomaria71 <brunomaria71@yahoo.com>
2022-05-31 15:45:56 -0400

    added form and sass

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   List,
 } from "@mui/material";
 import { getWishes, Wish } from "./services/wishes";
+import { WishForm } from "./components/WishForm";
 
 const theme = createTheme({
   typography: {
@@ -16,13 +17,13 @@ const theme = createTheme({
   palette: {
     mode: "dark",
     background: {
-      default: "black",
+      default: "#131924",
     },
     primary: {
-      main: "silver",
+      main: "#4d88ff",
     },
     secondary: {
-      main: "gold",
+      main: "#a9a9a9",
     },
   },
 });
@@ -34,9 +35,12 @@ export const App: FC = () => {
     getWishes().then(setWishes);
   }, []);
 
+  console.log(wishes);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <WishForm />
       <List>
         {wishes.map((wish) => {
           return <ListItem key={wish.name}>{wish.name}</ListItem>;
